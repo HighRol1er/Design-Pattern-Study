31936cd1c79b395cdde8c179e27288a2d1a2864b
HighRol1er <hunterjoe9999@gmail.com>
2025-08-29 17:11:59 +0900

    added: proxy Pattern

---
+++ proxy/index.js
@@ -0,0 +1,71 @@
+// Proxy 예제
+
+const person = {
+  name: "John Doe",
+  age: 42,
+  nationality: "American",
+};
+
+const personProxy = new Proxy(person, {
+  get: (obj, prop) => {
+    console.log(`The value of ${prop} is ${obj[prop]}`);
+  },
+  set: (obj, prop, value) => {
+    console.log(`Changed ${prop} from ${obj[prop]} to ${value}`);
+    obj[prop] = value;
+  },
+});
+
+personProxy.name;
+personProxy.age = 22;
+personProxy.nationality = "Korea";
+
+console.log(person);
+
+/****************************
+ * 유효성 검사를 넣은 Proxy ver *
+ ****************************/
+
+const personProxy2 = new Proxy(person, {
+  get: (obj, prop) => {
+    if (!obj[prop]) {
+      console.log(
+        `Hmm.. this property doesn't seem to exist on the target object`
+      );
+    } else {
+      console.log(`The value of ${prop} is ${obj[prop]}`);
+    }
+  },
+  set: (obj, prop, value) => {
+    if (prop === "age" && typeof value !== "number") {
+      console.log(`Sorry, you can only pass numeric values for age.`);
+    } else if (prop === "name" && value.length < 2) {
+      console.log(`You need to provide a valid name.`);
+    } else {
+      console.log(`Changed ${prop} from ${obj[prop]} to ${value}.`);
+      obj[prop] = value;
+    }
+  },
+});
+
+personProxy2.nonExistentProperty;
+personProxy2.age = "44";
+personProxy2.name = "";
+
+/***************
+ * Reflect ver *
+ ***************/
+
+const personProxy3 = new Proxy(person, {
+  get: (obj, prop) => {
+    console.log(`The value of ${prop} is ${Reflect.get(obj, prop)}`);
+  },
+  set: (obj, prop, value) => {
+    console.log(`Changed ${prop} from ${obj[prop]} to ${value}`);
+    return Reflect.set(obj, prop, value);
+  },
+});
+
+personProxy3.name;
+personProxy3.age = 43;
+personProxy3.name = "Jane Doe";
